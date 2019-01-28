f2a5d542cf0d18905d592409d7000c3be56682c9
AmplifiedHuman <AmplifiedHuman@gmail.com>
2019-01-28 00:59:57 +0000

    Removed unecessary files.

--- html/build/gwt/gen/com/google/gwt/lang/com_00046amplifiedhuman_00046drop_00046GdxDefinition__EntryMethodHolder.java
+++
@@ -1,6 +0,0 @@
-package com.google.gwt.lang;
-public class com_00046amplifiedhuman_00046drop_00046GdxDefinition__EntryMethodHolder {
-  public static final void init() {
-    // to be synthesized later
-  }
-}
